884b0a4b43b091e49f0ab04fca299263be4773f4
Pierohc <133154904+Pierohc@users.noreply.github.com>
2024-04-24 19:14:11 -0500

    AA

---
+++ GTICS-LAB4-20200814/src/test/java/com/example/gticslab420200814/GticsLab420200814ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gticslab420200814;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab420200814ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
