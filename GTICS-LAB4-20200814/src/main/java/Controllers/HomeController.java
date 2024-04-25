9d0f27ac1acee53d3e95c836fb10443606c81c4e
Pierohc <133154904+Pierohc@users.noreply.github.com>
2024-04-24 19:27:50 -0500

    asd

---
+++ GTICS-LAB4-20200814/src/main/java/Controllers/HomeController.java
@@ -0,0 +1,20 @@
+package Controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+
+@RequestMapping(value="/home",method= RequestMethod.GET)
+public class HomeController {
+
+    @GetMapping(value="")
+    public String home(){
+        return "home";
+    }
+
+
+
+
+}
