5af60e2640caff8703ec8cc57d821da94fa06de4
Pierohc <133154904+Pierohc@users.noreply.github.com>
2024-04-24 19:28:36 -0500

    aa

--- GTICS-LAB4-20200814/src/main/java/com/example/gticslab420200814/Controllers/HomeController.java
+++ GTICS-LAB4-20200814/src/main/java/com/example/gticslab420200814/Controllers/HomeController.java
@@ -1,4 +1,4 @@
-package Controllers;
+package com.example.gticslab420200814.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
